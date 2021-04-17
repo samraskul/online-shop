ad755b0fd867ef9f9730e3a575601540c33d8f36
sam <samraskul@hotmail.com>
2021-04-17 16:59:12 +0200

    compeleted product list component

--- resources/views/index/home.blade.php
+++ resources/views/index/home.blade.php
@@ -68,42 +68,7 @@
             <div class="main-card">
 
                 <!-- card start -->
-                <div class="card">
-                    <div class="card-header">
-                        <span class="fa fa-chart-line"></span>
-                        <div class="">Most views</div>
-                        <a href="" class="card-btn-more-products">
-                            <div> more products</div>
-                        </a>
-                    </div>
-                    <div class="card-body beauty-scroll">
-
-                        <div class="card-item-container">
-
-                            @foreach ($mostViewProducts as $product)
-                            <a href="{{ route('index.productSinglePage', optimizeUrl($product->id, $product->name)) }}" class="card-item-link">
-                                <div class="card-item">
-                                    {{-- @php
-                                        dd($product->images);
-                                    @endphp --}}
-                                    {{-- <div class="card-item-image"><img src="/images/0a0a1826f33eb6482ab0d604faf11e07.jpg" alt=""></div> --}}
-                                    <div class="card-item-image"> <img src="{{ $product->images->where('type', 'card')->first()->url }}" alt=""></div>
-                                    <div class="card-item-name">{{ $product->name }}</div>
-                                    <div class="card-item-prices">
-                                        <div class="card-item-old-price">{{ $product->price }}</div>
-                                        <div class="card-item-price">{{ $product->price - $product->discount }}</div>
-                                    </div>
-                                    <div class="card-btn-show-product">  Show product</div>
-                                </div>
-                            </a>    
-                            @endforeach
-                            
-                            
-
-                        </div>
-
-                    </div>
-                </div>
+                    <x-index.product-list :products="$mostViewProducts" cardTitle="Most views" cardLink="#1" />
                 <!-- card end -->
 
                 <!-- card start -->
